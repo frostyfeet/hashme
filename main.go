94dba8e8c1a7cdf1e6b767e5f7258ac46d6ca1e9
FrostyFeet <recogepelotas@gmail.com>
2017-02-26 18:18:32 -0800

    2nd commit

--- main.go
+++ main.go
@@ -1,24 +1,89 @@
 package main
 
-import "fmt"
-import "net/http"
+import (
+"fmt"
+"encoding/base64"
+"net/http"
+"crypto/md5"
+"crypto/sha1"
+"crypto/sha256"
+"golang.org/x/crypto/md4"
+"crypto/sha512"
+"encoding/hex"
+"encoding/json"
+"net/url"
+)
 
-func SimpleIndexHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello %s!", r.URL.Path[1:])
+type Hashlist struct { 
+  Hex string
+  URL string
+  Base64 string
+  MD4   string
+  MD5	string
+  SHA1	string
+  SHA224 string
+  SHA256 string
+  SHA384 string
+  SHA512 string
 }
 
-func HttpFileHandler(response http.ResponseWriter, request *http.Request) {
-	//fmt.Fprintf(w, "Hi from e %s!", r.URL.Path[1:])
-	http.ServeFile(response, request, "Index.html")
+func Handler(w http.ResponseWriter, r *http.Request) {
+    userString := ""
+    if len(r.URL.Path[1:]) < 2000 {
+        userString = r.URL.Path[1:]
+    }
+    temp := Hashlist {hash("hex",userString),hash("url",userString), hash("base64",userString), hash("md4",userString),hash("md5",userString),hash("sha1",userString), hash("sha224",userString),hash("sha256",userString), hash("sha384",userString),hash("sha512",userString)}
+
+    data, _ := json.MarshalIndent(temp,"","    ")
+	fmt.Fprintf(w, "%s\n", data)
+}
+
+func hash(t string, h string) (string) {
+    switch t {
+    case "url":
+        return url.QueryEscape (h)
+    case "md5":
+        hasher := md5.New()
+        hasher.Write([]byte(h))
+        return hex.EncodeToString(hasher.Sum(nil))
+    case "sha1":
+        hasher := sha1.New()
+        hasher.Write([]byte(h))
+        return hex.EncodeToString(hasher.Sum(nil))
+    case "sha224":
+		hasher := sha256.New224()
+        hasher.Write([]byte(h))
+        return hex.EncodeToString(hasher.Sum(nil))
+	case "sha256":
+		hasher := sha256.New()
+        hasher.Write([]byte(h))
+        return hex.EncodeToString(hasher.Sum(nil))
+    case "sha384":
+		hasher := sha512.New384()
+        hasher.Write([]byte(h))
+        return hex.EncodeToString(hasher.Sum(nil))
+    case "sha512":
+		hasher := sha512.New()
+        hasher.Write([]byte(h))
+        return hex.EncodeToString(hasher.Sum(nil))
+    case "md4":
+		hasher := md4.New()
+        hasher.Write([]byte(h))
+        return hex.EncodeToString(hasher.Sum(nil))
+    case "base64":
+        return base64.StdEncoding.EncodeToString([]byte(h))
+    case "hex":
+        src := []byte(h)
+        return hex.EncodeToString(src)
+    default: 
+        return "" 
+    }
 }
 
 func main() {
 
 	fmt.Println("Server Starting")
-	http.HandleFunc("/", SimpleIndexHandler)
-	http.HandleFunc("/index", HttpFileHandler)
+	http.HandleFunc("/", Handler)
 
-	//http.HandleFunc("/", indexTemplateHandler)
-
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe("127.0.0.1:3000", nil)
 }
